48bd8c2e595ee8bc00c9f0f5f0347fd1b7be5750
NehaSharma1607 <107467335+NehaSharma1607@users.noreply.github.com>
2024-09-27 16:17:59 +0530

    Add files via upload

---
+++ CopyFiles.py
@@ -0,0 +1,29 @@
+from pywinauto.application import Application
+import time
+
+
+password=input("Password: ")
+app = Application ().start (cmd_line=u'putty -ssh automation@10.58.220.139')
+putty = app.PuTTY
+putty.wait ('ready')
+time.sleep (1)
+putty.type_keys (password)
+putty.type_keys ("{ENTER}")
+time.sleep (1)
+putty.type_keys ("ls")
+putty.type_keys ("{ENTER}")
+putty.type_keys("cp log1/1.pdf targetdir/",with_spaces=True)
+putty.type_keys("{ENTER}")
+putty.type_keys("cp log2/2.pdf targetdir/",with_spaces=True)
+putty.type_keys("{ENTER}")
+putty.type_keys("cp log3/3.pdf targetdir/",with_spaces=True)
+putty.type_keys("{ENTER}")
+putty.type_keys("cp log4/4.pdf targetdir/",with_spaces=True)
+putty.type_keys("{ENTER}")
+putty.type_keys("cd targetdir",with_spaces=True)
+putty.type_keys("{ENTER}")
+putty.type_keys("ls")
+putty.type_keys("{ENTER}")
+time.sleep (5)
+putty.type_keys("exit")
+putty.type_keys("{ENTER}")
